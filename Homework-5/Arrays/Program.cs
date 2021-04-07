d25c55dfde128f5d930388e62e2b9407631b8675
Dean Derebeew <deanderebeew@ou.edu>
2021-04-06 19:26:46 -0500

    Finished Arrays part of HW#5, even though I added the files and I can see them on the Solution Explorer it says I havent added the files in an exception unhandled. I have emailed both of you but no response so hopefully I dont lose points. It is exactly how it was done in the video.

---
+++ Homework-5/Arrays/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] customernames = new string[1000];
+
+
+            double[] accountbalances = new double[1000];
+
+            string[] namelines = File.ReadAllLines("CustomerNames.CSV");
+            string[] balancesLines = File.ReadAllLines("AccountBalances.CSV");
+
+            for (int i = 1; i < namelines.Length; i++)
+            {
+                if (string.IsNullOrWhiteSpace(namelines[i]) == true)
+                {
+                    continue;
+                }
+
+                customernames[i - 1] = namelines[i];
+
+                string balance = balancesLines[i];
+                balance = balance.Replace("$", "");
+                accountbalances[i - 1] = Convert.ToDouble(balance);
+
+            }
+
+
+            Console.WriteLine("Whose account do you want to look up? >>");
+            foreach (string name in customernames)
+            {
+                Console.WriteLine(name);
+            }
+
+            string answer = Console.ReadLine();
+
+            Console.WriteLine();
+            for (int i = 0; i < customernames.Length; i++)
+            {
+                if (answer.ToLower() == customernames[i].ToLower())
+                {
+                    Console.WriteLine($"{answer} has a balance of {accountbalances[i].ToString("C")}");
+                }
+            }
+        }
+    }
+}
